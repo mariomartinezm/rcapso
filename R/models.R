4de68d68d5abc54111924dfb5fbb1c05f4519a69
mariomm <mariomartinezmolina@live.com>
2022-08-16 10:54:05 -0500

    Add models.R
    This file contains functions that simulate the mean field equations of
    the capso model.

---
+++ R/models.R
@@ -0,0 +1,58 @@
+#' Plots the mean field approximation of the capso model.
+#' @param N The number of iterations to simulate.
+#' @param psi0 The initial density of the prey population.
+#' @param phi0 The initial density of the predator population.
+#' @param alpha The intraspecific competition coefficient.
+#' @param ey The reproductive capacity capacity of preys.
+#' @param ry The radius of the prey's reproduction neighborhood.
+#' @param ez The reproductive capacity capacity of predators.
+#' @param rz The radius of the predator's reproduction neighborhood.
+#' @param a The ordinate of the line that defines predator mortality.
+#' @param b The slope of the line that defines predator mortality.
+#' @param c The slope of the line that defines prey mortality.
+#' @return The numeric vectors of preys and predators.
+#' @export
+capso_mean_field <- function(num_iter = 100, psi0 = 1, phi0 = 0.01, alpha = 0.1,
+                             ey = 1, ry = 1, ez = 1, rz = 1,
+                             a = -1, b = 1, c = 1, d = 0) {
+  # Initialize data structures
+  index_set <- seq(from=1, to=num_iter, by=1)
+  psi       <- numeric(length(index_set))
+  phi       <- numeric(length(index_set))
+
+  # Calculate the probability of an 'event' occurring in the neighboord of a
+  # prey or predator
+  card_mry  <- (2 * ry + 1) ^ 2 - 1
+  py        <- 1 / card_mry
+  card_mrz  <- (2 * rz + 1) ^ 2 - 1
+  pz        <- 1 / card_mrz
+
+  # Initialize population densities and simulate the model
+  psi[1] <- psi0
+  phi[1] <- phi0
+
+  for (t in seq_len(length(index_set) - 1)) {
+    # Intraspecific competition
+    psi_ic <- psi[t] - alpha * psi[t] ^ 2
+
+    # Reproduction of predators
+    num_preds          <- card_mrz * phi[t]
+    max_num_of_births  <- ez * num_preds
+    phi_r              <- phi[t] + (1 - phi[t]) * (1 - (1 - pz) ^
+                                                   max_num_of_births)
+
+    # Death of Predators
+    phi[t + 1] <- phi_r - (b + a * psi_ic) * phi_r
+
+    # Death Of Preys
+    psi_d <- psi_ic - (d + c * phi[t + 1]) * psi_ic
+
+    # Reproduction of preys
+    num_preys         <- card_mry * psi_d
+    max_num_of_births <- ey * num_preys
+    psi[t + 1]        <- psi_d + (1 - psi_d) * (1 - (1 - py) ^
+                                                max_num_of_births)
+  }
+
+  list(psi, phi)
+}
