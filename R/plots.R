50e4c010dcca8f087d7ecc02c378bbe7a9e88081
mariomm <mariomartinezmolina@live.com>
2022-09-22 12:04:44 -0500

    Update plots.R
    Add function capso_plot_prey_pred_data(). This function offers an easy way to
    plot prey-predator data.

--- R/plots.R
+++ R/plots.R
@@ -14,6 +14,27 @@ theme <- ggplot2::theme(plot.title = ggplot2::element_text(face = "bold.italic",
                         panel.grid.minor.x = ggplot2::element_line(color = "lightgray"),
                         panel.grid.minor.y = ggplot2::element_line(color = "lightgray"))
 
+#' Plots the time series of preys and predators.
+#' @param data A two-column matrix containing the data of preys and predators.
+#' @param title The title for the plot.
+#' @param xlabel The label for the x axis.
+#' @param ylabel The label for the y axis.
+#' @export
+capso_plot_prey_pred_data <- function(data, title = "",
+                                      xlabel = "", ylabel = "") {
+  index_set <- 1:nrow(data)
+
+  ggplot2::ggplot() +
+    ggplot2::geom_line(data = data,
+                       ggplot2::aes(index_set, y = data[, 1]),
+                       color = "#69b3a2", size = 1) +
+    ggplot2::geom_line(data = data,
+                       ggplot2::aes(index_set, y = data[, 2]),
+                       color = "red", size = 1) +
+    ggplot2::labs(x = xlabel, y = ylabel, title = title) +
+    theme
+}
+
 #' Plots the phase plot of two time series contained in parameter data
 #' @param data A two-column matrix containing two time series.
 #' @param title The title for the plot.
