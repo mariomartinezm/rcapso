ab6e3428f1fa8659b5f8bfc57ff4a74a1ce9586c
mariomm <mariomartinezmolina@live.com>
2022-09-27 14:02:14 -0500

    Update plots.R
    Clean code.

--- R/plots.R
+++ R/plots.R
@@ -1,18 +1,26 @@
-theme <- ggplot2::theme(plot.title = ggplot2::element_text(face = "bold.italic",
-                                                           size = 14,
-                                                           color = "black"),
-                        axis.title = ggplot2::element_text(face = "bold",
-                                                           size = 12,
-                                                           color = "black"),
-                        axis.text = ggplot2::element_text(face = "bold",
-                                                          size = 9,
-                                                          color = "black"),
-                        panel.background = ggplot2::element_rect(fill = "white",
-                                                                 color = "white"),
-                        panel.grid.major.x = ggplot2::element_line(color = "lightgray"),
-                        panel.grid.major.y = ggplot2::element_line(color = "lightgray"),
-                        panel.grid.minor.x = ggplot2::element_line(color = "lightgray"),
-                        panel.grid.minor.y = ggplot2::element_line(color = "lightgray"))
+theme <- ggplot2::theme(plot.title =
+                          ggplot2::element_text(face = "bold.italic",
+                                                size = 14,
+                                                color = "black"),
+                        axis.title =
+                          ggplot2::element_text(face = "bold",
+                                                size = 12,
+                                                color = "black"),
+                        axis.text =
+                          ggplot2::element_text(face = "bold",
+                                                size = 9,
+                                                color = "black"),
+                        panel.background =
+                          ggplot2::element_rect(fill = "white",
+                                                color = "white"),
+                        panel.grid.major.x =
+                          ggplot2::element_line(color = "lightgray"),
+                        panel.grid.major.y =
+                          ggplot2::element_line(color = "lightgray"),
+                        panel.grid.minor.x =
+                          ggplot2::element_line(color = "lightgray"),
+                        panel.grid.minor.y =
+                          ggplot2::element_line(color = "lightgray"))
 
 #' Plots the time series of preys and predators.
 #' @param data A two-column matrix containing the data of preys and predators.
@@ -41,10 +49,13 @@ capso_plot_prey_pred_data <- function(data, title = "",
 #' @param xlabel The label for the x axis.
 #' @param ylabel The label for the y axis.
 #' @export
-capso_phase_plot <- function(data, title="", xlabel="", ylabel="") {
+capso_plot_phase <- function(data, title = "", xlabel = "", ylabel = "") {
   ggplot2::ggplot(data = data, ggplot2::aes(x = data[, 1], y = data[, 2])) +
-    ggplot2::geom_path(color="black") +
-    ggplot2::geom_point(shape=21, color="black", fill="#69b3a2", size=4) +
+    ggplot2::geom_path(color = "black") +
+    ggplot2::geom_point(shape = 21,
+                        color = "black",
+                        fill = "#69b3a2",
+                        size = 4) +
     ggplot2::labs(x = xlabel, y = ylabel, title = title) +
     theme
 }
@@ -57,10 +68,9 @@ capso_phase_plot <- function(data, title="", xlabel="", ylabel="") {
 #' power spikes.
 #' @export
 capso_plot_fourier_spectrum <- function(x, method_spec) {
-  if(method_spec) {
+  if (method_spec) {
     spec_out <- stats::spectrum(x, method = "ar")
-  }
-  else {
+  } else {
     spec_out <- stats::spectrum(x, method = "pgram")
   }
 
@@ -70,17 +80,18 @@ capso_plot_fourier_spectrum <- function(x, method_spec) {
 
   # Sort cycles in order of magnitude of power spikes
   hold <- matrix(0, (length(power) - 2), 1)
-  for(i in 1:(length(power) - 2)) {
-    max1     <- if(power[i + 1] > power[i] && power[i + 1] > power[i + 2]) 1 else (0)
-    hold[i,] <- max1
+  for (i in 1:(length(power) - 2)) {
+    max1      <- if (power[i + 1] > power[i] &&
+                     power[i + 1] > power[i + 2]) 1 else (0)
+    hold[i, ] <- max1
   }
 
-  max <- which(hold == 1) + 1
-  power_max <- power[max]
-  cycle_max <- cycle[max]
-  o <- order(power_max, decreasing = TRUE)
+  max         <- which(hold == 1) + 1
+  power_max   <- power[max]
+  cycle_max   <- cycle[max]
+  o           <- order(power_max, decreasing = TRUE)
   cycle_max_o <- cycle_max[o]
-  results <- list(cycle_max_o)
+  results     <- list(cycle_max_o)
 
   return(results)
 }
